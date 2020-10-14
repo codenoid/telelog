29bf0d643c4db02803dc841a054abcf4b354fe8b
kai <14269809+codenoid@users.noreply.github.com>
2020-10-15 03:09:38 +0700

    add cmd, update deps

---
+++ cmd/cmd.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/codenoid/telelog"
+
+	"github.com/hpcloud/tail"
+)
+
+var file string
+var level string
+
+func init() {
+	fmt.Println("Telelog, make sure you already set these env : ")
+	fmt.Println("TELELOG_BOT_TOKEN")
+	fmt.Println("TELELOG_APP_NAME")
+	fmt.Println("TELELOG_DEBUG_MODE (optional)")
+	fmt.Println("TELELOG_RECIPIENT_LIST")
+
+	flag.StringVar(&file, "file", "", "path to file to tail")
+	flag.StringVar(&level, "level", "debug", "level (error|warn|info|debug")
+
+	flag.Parse()
+
+}
+
+func main() {
+
+	logger := telelog.LoggerNew()
+
+	logger.SetAppName(file)
+	if level == "debug" {
+		logger.SetDebug(true)
+	}
+
+	if err := logger.Connect(); err != nil {
+		panic(err)
+	}
+
+	t, err := tail.TailFile(file, tail.Config{Follow: true, MustExist: true, ReOpen: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for line := range t.Lines {
+		switch level {
+		case "error":
+			logger.Error(line.Text)
+		case "warn", "warning":
+			logger.Warn(line.Text)
+		case "info":
+			logger.Info(line.Text)
+		default:
+			logger.Debug(line.Text)
+		}
+	}
+
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.15
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/hpcloud/tail v1.0.0
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	github.com/withmandala/go-log v0.1.0
 	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
